a9c049e3ef9185e9b3bf5726bb959e16d3d291da
roteKlaue <janoolhd@gmail.com>
2023-01-06 00:49:12 +0100

    fixed wrong colors

--- src/Types/Level.ts
+++ src/Types/Level.ts
@@ -8,10 +8,10 @@ declare type Level = {
 
 const Levels = {
     error: { name: "error", color: Colors.red, level: 1 } as Level,
-    info: { name: "info", color: Colors.red, level: 4 } as Level,
-    log: { name: "log", color: Colors.red, level: 3 } as Level,
-    warn: { name: "warn", color: Colors.red, level: 2 } as Level,
-    success: { name: "success", color: Colors.red, level: 5 } as Level,
+    info: { name: "info", color: Colors.white, level: 4 } as Level,
+    log: { name: "log", color: Colors.white, level: 3 } as Level,
+    warn: { name: "warn", color: Colors.brightRed, level: 2 } as Level,
+    success: { name: "success", color: Colors.green, level: 5 } as Level,
 }
 
 export {
